c78814a92cd67b8437c70d2ac5ff441366fec1f0
Ryan <ryanpdwyer@gmail.com>
2017-07-03 17:07:08 -0400

    Removed test for function which was deleted

--- newtex/tests/test_git.py
+++ newtex/tests/test_git.py
@@ -1,11 +1,6 @@
 import unittest
-from newtex._git import parse_git_config
 
 
-class Test_parse_git_config(unittest.TestCase):
-
-    def test_equals_signs(self):
-        config_with_equals = "test.parameter=this=has=equals=signs"
-        self.assertEquals(parse_git_config(config_with_equals),
-                          {'test.parameter':
-                          'this=has=equals=signs'})
+class TestExample(unittest.TestCase):
+    def test_example(self):
+        self.assertEquals(0, 0)
